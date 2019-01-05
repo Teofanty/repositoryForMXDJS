3d3317b2155694ceb8c3192589caf0fdc7a88921
teo <414615792@qq.com>
2019-01-05 14:45:29 +0800

    修改购买双倍经验卡逻辑

--- src/main/resources/newJS/购买现金物品脚本.js
+++ src/main/resources/newJS/购买现金物品脚本.js
@@ -26,18 +26,23 @@ function action(mode, type, selection) {
             for (i = 0; i < 10; i++) {
                 text += "";
             }
-            text += "\t\t\t#e欢迎来到#b中心 #k!#n\r\n\r\n";
+            text += "\t\t\t#e欢迎来到#b现金道具中心 #k!#n\r\n\r\n";
             text += "#L1#使用200点券购买双倍经验卡3小时(双倍卡一天只能购买一次)#l\r\n\r\n";
             text += "#L2#使用200抵用券购买双倍经验卡3小时(双倍卡一天只能购买一次)#l\r\n\r\n";
 
             cm.sendSimple(text);
         }
         if (status == 1) {
+            if(cm.getPlayer().getOneTimeLog("doubleExpCard") >= 1){
+                cm.sendOk("双倍经验卡一天只能购买一次，请明天再来");
+                cm.dispose();
+            }
             if (selection == 1) {
                 if (cm.getPlayer().getCSPoints(1) < 200) {
                     cm.sendOk("您的点券不够，请核对后重试");
                     cm.dispose();
                 }
+                cm.getPlayer().setBossLog('doubleExpCard');
                 cm.gainDJ(-200);
                 cm.gainItem(5211047);
                 cm.dispose();
@@ -46,6 +51,7 @@ function action(mode, type, selection) {
                     cm.sendOk("您的抵用券不够，请核对后重试");
                     cm.dispose();
                 }
+                cm.getPlayer().setBossLog('doubleExpCard');
                 cm.gainD(-200);
                 cm.gainItem(5211047);
                 cm.dispose();
